2f1fbf8a42b23b5f6a767c214be6ea861afcf1c0
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-05-27 16:42:59 +0100

    Norm errors and compilation errors

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 15:10:22 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/05/27 16:26:25 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/05/27 16:35:04 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ char			*get_next_line(int fd);
 char			*ft_strchr(const char *str, int chr);
 char			*ft_strjoin(const char *s1, const char *s2);
 void			*ft_calloc(size_t nmemb, size_t size);
-static char		*ft_get_stash(char *line);
+//static char		*ft_get_stash(char *line);
 char			*ft_get_all(int fd, char *buffer, char *stash);
 size_t			ft_strlen(const char *str);
 int				ft_check_newline(char *str);
